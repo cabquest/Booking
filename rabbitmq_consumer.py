0d9c592a90c517b18153b38e74447e0eacdec666
Abdul Samad <abdulsamad52556@gmail.com>
2024-08-12 23:02:21 +0530

    queue_adjusted

--- rabbitmq_consumer.py
+++ rabbitmq_consumer.py
@@ -10,7 +10,7 @@ app = create_app()
 app.app_context().push()
 
 
-rabbitmq_host = 'localhost'  
+rabbitmq_host = 'host.docker.internal'  
 rabbitmq_port = 5672
 rabbitmq_user = 'guest'  
 rabbitmq_password = 'guest'  
